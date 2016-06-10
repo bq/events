941e549a0d584a33c14873f51463a5d35d1f636e
samuelmartingc <samuelmartingc@gmail.com>
2016-06-10 13:05:41 +0200

    updated AssetEvents with new fields like filters
    Change-Id: I9858e122a4a66b01bca7bec02f6e902a8eb614d3

--- src/main/java/com/bq/corbel/event/AssetsEvent.java
+++ src/main/java/com/bq/corbel/event/AssetsEvent.java
@@ -2,6 +2,7 @@ package com.bq.corbel.event;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import com.bq.corbel.eventbus.EventWithSpecificDomain;
@@ -58,6 +59,9 @@ public class AssetsEvent extends EventWithSpecificDomain {
         private Date expire;
         private String name;
         private String productId;
+        private Map<String, Boolean> filterResults;
+        private List<String> filters;
+        private Map<String, Object> properties;
 
         public EventAsset() {}
 
@@ -67,6 +71,30 @@ public class AssetsEvent extends EventWithSpecificDomain {
             this.expire = expire;
             this.name = name;
             this.productId = productId;
+        }
+
+        public EventAsset(Set<String> scopes, Date expire, String name, String productId, Map<String, Boolean> filterResults,
+                List<String> filters, Map<String, Object> properties) {
+            super();
+            this.scopes = scopes;
+            this.expire = expire;
+            this.name = name;
+            this.productId = productId;
+            this.filterResults = filterResults;
+            this.filters = filters;
+            this.properties = properties;
+        }
+
+        public Map<String, Boolean> getFilterResults() {
+            return filterResults;
+        }
+
+        public List<String> getFilters() {
+            return filters;
+        }
+
+        public Map<String, Object> getProperties() {
+            return properties;
         }
 
         public Set<String> getScopes() {
@@ -85,33 +113,38 @@ public class AssetsEvent extends EventWithSpecificDomain {
             return productId;
         }
 
-        @Override
-        public boolean equals(Object o) {
+        @Override public boolean equals(Object o) {
             if (this == o)
                 return true;
-            if (!(o instanceof EventAsset))
+            if (o == null || getClass() != o.getClass())
                 return false;
 
             EventAsset that = (EventAsset) o;
 
+            if (scopes != null ? !scopes.equals(that.scopes) : that.scopes != null)
+                return false;
             if (expire != null ? !expire.equals(that.expire) : that.expire != null)
                 return false;
             if (name != null ? !name.equals(that.name) : that.name != null)
                 return false;
             if (productId != null ? !productId.equals(that.productId) : that.productId != null)
                 return false;
-            if (scopes != null ? !scopes.equals(that.scopes) : that.scopes != null)
+            if (filterResults != null ? !filterResults.equals(that.filterResults) : that.filterResults != null)
                 return false;
+            if (filters != null ? !filters.equals(that.filters) : that.filters != null)
+                return false;
+            return properties != null ? properties.equals(that.properties) : that.properties == null;
 
-            return true;
         }
 
-        @Override
-        public int hashCode() {
+        @Override public int hashCode() {
             int result = scopes != null ? scopes.hashCode() : 0;
             result = 31 * result + (expire != null ? expire.hashCode() : 0);
             result = 31 * result + (name != null ? name.hashCode() : 0);
             result = 31 * result + (productId != null ? productId.hashCode() : 0);
+            result = 31 * result + (filterResults != null ? filterResults.hashCode() : 0);
+            result = 31 * result + (filters != null ? filters.hashCode() : 0);
+            result = 31 * result + (properties != null ? properties.hashCode() : 0);
             return result;
         }
 
