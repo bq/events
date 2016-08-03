50a5cd4e9a3ac4467ce3a663dec9db1e45176588
samuelmartingc <samuelmartingc@gmail.com>
2016-08-03 08:41:53 +0200

    emails are optional when a user is created
    Change-Id: Ifea83a3d5bfe2df66122c1eae4acf50b52590987

--- src/main/java/com/bq/corbel/event/UserCreatedEvent.java
+++ src/main/java/com/bq/corbel/event/UserCreatedEvent.java
@@ -8,6 +8,7 @@ import java.util.Set;
  * @author Alberto J. Rubio
  */
 public class UserCreatedEvent extends UserEvent {
+    private boolean avoidNotification;
 
     public UserCreatedEvent() {}
 
@@ -15,13 +16,23 @@ public class UserCreatedEvent extends UserEvent {
         super(domainId, userId, email, username, null, null, null, null, null, null, null, null);
     }
 
-    public UserCreatedEvent(String domainId, String userId, String email, String username, String firstName, String lastName,
-            String profileUrl, String phoneNumber, String country, Map<String, Object> properties, Set<String> scopes, Set<String> groups) {
+    public UserCreatedEvent(String domainId, String userId, String email, String username,
+            String firstName, String lastName, String profileUrl, String phoneNumber,
+            String country, Map<String, Object> properties, Set<String> scopes,
+            Set<String> groups, boolean avoidNotification) {
         super(domainId, userId, email, username, firstName, lastName, profileUrl, phoneNumber, country, properties, scopes, groups);
     }
 
     @Override
     public UserEventType getType() {
         return UserEventType.CREATION;
+    }
+
+    public boolean isAvoidNotification() {
+        return avoidNotification;
+    }
+
+    public void setAvoidNotification(boolean avoidNotification) {
+        this.avoidNotification = avoidNotification;
     }
 }
