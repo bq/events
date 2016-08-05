d20110b6d33b84e1e2cd367b301d3785acd1a7cb
samuelmartingc <samuelmartingc@gmail.com>
2016-08-05 08:25:36 +0200

    emails are optional when a user is deleted
    Change-Id: Iba5da700fde73876712bfac0ca5b91b4afd06026

--- src/test/java/com/bq/corbel/event/EventSerializationTest.java
+++ src/test/java/com/bq/corbel/event/EventSerializationTest.java
@@ -90,7 +90,7 @@ public class EventSerializationTest {
 
     @Test
     public void testUserDeletedEvent() {
-        assertThanCanBeSendAndRetriveInEventBus(new UserDeletedEvent("USER_ID", "FIRST_NAME","LAST_NAME","EMAIL@EMAIL.COM", "DOMAIN"));
+        assertThanCanBeSendAndRetriveInEventBus(new UserDeletedEvent("USER_ID", "FIRST_NAME","LAST_NAME","EMAIL@EMAIL.COM", "DOMAIN", false));
     }
 
     @Test
